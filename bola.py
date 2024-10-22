343072b16957d904ed37b497ddd7a074d0294d4f
= <=>
2024-10-22 09:40:47 +0700

    bola

---
+++ bola.py
@@ -0,0 +1,15 @@
+#dikerjakan oleh dika prayoga gunawan
+#tanggal pengerjaan 22 oktober 2024
+#program menghitung bola
+
+print("="*40)
+print("bola")
+print("="*40)
+
+r = int(input("jari-jari\t: "))
+
+l = 4 * 3.14 * r * r
+v = 4/3 * 3.14 * r *r * r 
+
+print(f'luas : {round(l,2)}')
+print(f'volume : {round(v,2)}')
