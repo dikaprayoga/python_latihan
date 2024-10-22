c657d49f4cc9f2744c5811dad29e0b7c7410b659
= <=>
2024-10-22 09:27:23 +0700

    trapesium

---
+++ trapesium.py
@@ -0,0 +1,19 @@
+#dikerjakan oleh dika prayoga gunawan
+#tanggal pengerjaan 22 oktober 2024
+#program menghitung trapesium
+
+print("="*40)
+print("trapesiumsssss")
+print("="*40)
+
+sisi1 = int(input("sisii sejajar 1\t\t: "))
+sisi2 = int(input("sisii sejajar 2\t\t: "))
+sisi3 = int(input("sisi tidak sejajar 1\t: "))
+sisi4 = int(input("sisi tidak sejajar 2\t: "))
+t = int(input("Tinggi\t\t\t: "))
+
+l = 1/2 * (sisi1 + sisi2) * t
+k = sisi1 + sisi2 + sisi3 + sisi4
+
+print(f"Luas\t\t\t: {1}")
+print(f"Keliling\t\t: {k}")
