c022ef100c33da3519285a65bd9d7b28c40be8c7
= <=>
2024-11-08 07:28:36 +0700

    lampu lalu lintas

---
+++ lampulalulintas.py
@@ -0,0 +1,10 @@
+warna = input("Masukan warna lampu lalu lintas : ")
+
+if warna == "merah"  :
+    print("berhenti")
+elif warna == "kuning" :
+    print("bersedia")
+elif warna == "hijau" :
+    print("berjalan")
+else:
+    print("bukan warna lampu lalu lintas")
