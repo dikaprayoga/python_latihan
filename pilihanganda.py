8aa267d17303b6b97ecf4e84e7b5c14b6bfcee82
= <=>
2024-11-28 19:22:09 +0700

    ganda

---
+++ pilihanganda.py
@@ -0,0 +1,12 @@
+print("SIAPA KIPER PEMAIN PERSIB BANDUNG SEKARANG")
+print('''
+    A.izzul
+    B.keanu
+    C.Dika
+    D.Kevin Mendoza''')
+
+a=(input('Masukan Jawaban: '))
+if a == 'D':
+    print('Benar')
+else:
+    print ('Salah,Jawabanya adalah, D')
