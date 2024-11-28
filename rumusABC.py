65e37aafcda96679a42ac61bc1b7e8b4cbd5b1ce
= <=>
2024-11-28 19:34:54 +0700

    rumusABC

---
+++ rumusABC.py
@@ -0,0 +1,10 @@
+print(35*"\033[34m=")
+print("\033[31mPROGRAM ABC".center(40))
+print(35*"\033[34m=")
+
+a = float(input("masukan nilai a = "))
+b = float(input("masukan nilai b = "))
+c = float(input("masukan nilai c = "))
+
+x = (-b) + 2  * (c**2) * + 4 * a * b / 2 * c 
+print(f"hasil dari rumus abc adalah = {x:.2f}")
