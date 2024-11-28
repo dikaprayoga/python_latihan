b16075f565838272d18fc0f5ec8fa63c13d1c2f5
= <=>
2024-11-28 19:44:18 +0700

    bintangultramen

---
+++ bintangultramen.py
@@ -0,0 +1,9 @@
+user = int(input("masukan nilai bintang = "))
+print("segitiga biasa")
+for i in range(user):
+    print("*"*i)
+    i += 1
+no = 1
+for no in range(user):
+    print("*"*user)
+    user -= 16
